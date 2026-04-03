8576bbf959c01dc0d5e52035ce2d29857f9ea23c
sriram-768 <sriram85763@gmail.com>
2026-04-03 11:48:14 +0530

    deleted

--- frontend/src/api/auth.js
+++
@@ -1,9 +0,0 @@
-import { apiClient } from './client';
-
-export const login = (email, password) => {
-  return apiClient('/auth/login', { body: { email, password } });
-};
-
-export const signup = (username, email, password) => {
-  return apiClient('/auth/signup', { body: { username, email, password } });
-};
--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "web_public_ip" {
-  description = "Public IP of the web server"
-  value       = aws_instance.web.public_ip
-}
-
-output "rds_endpoint" {
-  description = "Endpoint of the RDS MySQL instance"
-  value       = aws_db_instance.mysql_db.endpoint
-}
